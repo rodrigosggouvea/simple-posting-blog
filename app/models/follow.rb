4263b57b14b46e8e47cc174ba1c58ce23205b1f3
Rodrigo Gouvea <rodrigosggouveaq@gmail.com>
2022-02-18 08:41:34 -0300

    feat(api): multiple follow validation

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,6 +2,7 @@ class Follow < ApplicationRecord
   belongs_to :follower, class_name: 'User'
   belongs_to :followed, class_name: 'User'
   validate :self_follow
+  validates :followed, uniqueness: { scope: :follower_id }
 
   def self_follow
     errors.add(:followed, 'users cannot follow themselves') if follower == followed
--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 RSpec.describe(Follow, type: :model) do
   describe 'validations' do
+    let!(:a) { User.create(username: 'abc') }
+    let!(:b) { User.create(username: 'bcd') }
+
     it 'does not allow self follow' do
-      a = User.create(username: 'abc')
-      b = User.create(username: 'bcd')
       expect(Follow.new(follower: a, followed: b)).to be_valid
       expect(Follow.new(follower: a, followed: a)).not_to be_valid
+    end
+
+    it 'does not allow double follow' do
+      Follow.create(follower: a, followed: b)
+      expect(Follow.new(follower: a, followed: b)).not_to be_valid
     end
   end
 
