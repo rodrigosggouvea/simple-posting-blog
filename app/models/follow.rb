bbc13686e3f5a5894ae3d30c9a88f10cf71c9e7e
Rodrigo Gouvea <rodrigosggouveaq@gmail.com>
2022-02-18 07:45:19 -0300

    feat(api): follow validation

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,9 @@
 class Follow < ApplicationRecord
   belongs_to :follower, class_name: 'User'
   belongs_to :followed, class_name: 'User'
+  validate :self_follow
+
+  def self_follow
+    errors.add(:followed, 'users cannot follow themselves') if follower == followed
+  end
 end
--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe(Follow, type: :model) do
+  describe 'validations' do
+    it 'does not allow self follow' do
+      a = User.create(username: 'abc')
+      b = User.create(username: 'bcd')
+      expect(Follow.new(follower: a, followed: b)).to be_valid
+      expect(Follow.new(follower: a, followed: a)).not_to be_valid
+    end
+  end
+
   describe 'relationships' do
     it { should belong_to(:follower) }
     it { should belong_to(:followed) }
