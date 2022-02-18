f47f35c7a5fc8a467fd49746fc4d4b11b13f6c98
Rodrigo Gouvea <rodrigosggouveaq@gmail.com>
2022-02-18 09:34:02 -0300

    feat(api): daily post count validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,13 @@ class Post < ApplicationRecord
 
   validates :body, presence: true, if: -> { parent_id.blank? }
   validates :body, length: { maximum: 777 }
+
+  validate :user_post_limit
+
+  scope :today, -> { where('DATE(created_at) = ?', Date.today) }
+  scope :by_user, ->(user_id) { where(user: user_id) }
+
+  def user_post_limit
+    errors.add(:user, 'users cannot post more than 5 times a day') if Post.by_user(user_id).today.count == 5
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,6 +4,12 @@ RSpec.describe(Post, type: :model) do
   describe 'validations' do
     it { should validate_presence_of(:body) }
     it { should validate_length_of(body: { maximum: 777 }) }
+
+    it 'should deny more than 5 posts a day per user' do
+      user = User.create(username: 'testuser')
+      5.times { user.posts.create(body: 'tests post') }
+      expect(user.posts.build(body: 'invalid post')).not_to be_valid
+    end
   end
 
   describe 'relationships' do
